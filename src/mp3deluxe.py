2abb151c1343c32e34bfb0d3ada52336274c4f74
Mathieu Lecarme <mathieu@garambrogne.net>
2010-09-25 22:22:28 +0200

    object is power

--- src/mp3deluxe.py
+++ src/mp3deluxe.py
@@ -5,15 +5,76 @@ __author__ = "mathieu@garambrogne.net"
 
 import subprocess
 import re
+import os
+import os.path
+import shutil
+import time
 
-def cdda():
+class MultiProcess(object):
+	def __init__(self, processors = 2):
+		self.processors = processors
+		self.cmds = []
+		self.actions = []
+	def cmd(self, cmd):
+		self.cmds.append(cmd)
+	def wait(self):
+		while len(self.cmds):
+			if len(self.actions) < self.processors:
+				cmd = self.cmds.pop()
+				print cmd
+				self.actions.append(subprocess.Popen(cmd))
+			for action in self.actions:
+				if action.poll() != None:
+					self.actions.remove(action)
+			time.sleep(1)
+
+class Tracks(object):
+	def __init__(self, cdda, wav):
+		self.path = os.path.join(cdda.path, wav)
+		self.cdda = cdda
+		root, ext = os.path.splitext(wav)
+		s = root.split(' ')
+		self.number = int(s[0])
+		self.title = ' '.join(s[1:])
+	def __repr__(self):
+		return '<Track #%i "%s">' % (self.number, self.title)
+
+class CDDA(object):
+	def __init__(self, path):
+		self.path = path
+		self.title = path.split('/')[-1]
+	def __iter__(self):
+		for wav in os.listdir(self.path):
+			if wav[0] != '.':
+				yield Tracks(self, wav)
+	def __repr__(self):
+		return '<CDDA "%s">' % self.title
+
+def cddas():
+	"find all audio cd available"
 	cd = re.compile(r'(/dev/disk[0-9]+) on (/Volumes/.*?) \(.*?\)')
 	c = subprocess.Popen('mount | grep cddafs', stdout=subprocess.PIPE, shell=True)
 	c.wait()
 	for line in c.stdout.read().split("\n")[:-1]:
 		m = cd.match(line)
 		if m != None:
-			yield m.group(1),m.group(2)
+			yield CDDA(m.group(2))
+
+
+def to_mp3(cd):
+	"convert an audio cd to mp3"
+	m = MultiProcess()
+	for wav in os.listdir(cd):
+		if wav[0] != '.':
+			p = os.path.join(cd, wav)
+			root, ext = os.path.splitext(wav)
+			c = subprocess.Popen(['lame', '-b320', '-q0', '-m', 's', '--nohist', '--disptime', '2', p, '%s.mp3' % root])
+			print wav
+			c.wait()
+	m.wait()
 
 if __name__ == '__main__':
-	print list(cdda())
+	for cd in cddas():
+		print cd
+		for track in cd:
+			print '\t', track
